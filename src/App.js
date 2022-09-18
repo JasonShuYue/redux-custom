b975b15cfe08421eae04d5310a70328ff3637b90
Jason_Shu <shuyue524@gmail.com>
2022-09-18 19:06:28 +0800

    05 - 通过发布订阅模式来实现精准render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 const AppContext = React.createContext(null);
 
@@ -9,10 +9,18 @@ const store = {
       age: 18,
     },
   },
-
+  listeners: [],
+  subscribe: (fn) => {
+    store.listeners.push(fn);
+    return () => {
+      const index = store.listeners.indexOf(fn);
+      store.listeners.splice(index, 1);
+    };
+  },
   setState: (newState) => {
-    console.log(newState);
     store.state = newState;
+
+    store.listeners.forEach((fn) => fn(store.state));
   },
 };
 
@@ -51,12 +59,6 @@ const 小儿子 = () => {
   return <section>小儿子</section>;
 };
 
-const User = () => {
-  console.log("User 执行了", Math.random());
-  const { state } = useContext(AppContext);
-  return <div>User: {state.user.name}</div>;
-};
-
 const reducer = (state, { type, payload }) => {
   if (type === "updateUser") {
     return {
@@ -73,17 +75,25 @@ const reducer = (state, { type, payload }) => {
 
 const connect = (Component) => {
   return (props) => {
-    const { state, setState } = useContext(AppContext);
+    const { state, setState, listeners } = useContext(AppContext);
     const [, update] = useState({});
+
+    useEffect(() => {
+      listeners.push(() => update({}));
+    }, []);
 
     const dispatch = (action) => {
       setState(reducer(state, action));
-      update({});
     };
 
     return <Component {...props} state={state} dispatch={dispatch} />;
   };
 };
+
+const User = connect(({ state }) => {
+  console.log("User 执行了", Math.random());
+  return <div>User: {state.user.name}</div>;
+});
 
 const UserModifier = connect(({ state, dispatch }) => {
   console.log("UserModifier 执行了", Math.random());
